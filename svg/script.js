6cf405d8fb4af3e6185f5ff142f5f452d242cfca
Victor Rios Saucedo <36146644+Rotooo@users.noreply.github.com>
2022-12-15 17:38:58 +0000

    Agregar script

---
+++ svg/script.js
@@ -0,0 +1,11 @@
+import Access from './access.svg';
+
+export const programs = [
+  {
+    id: 1,
+    title: 'Windows 7/8/8.1/10',
+    icon: Access,
+    porcent: '88%',
+    color: '#2196F3',
+  },
+];
